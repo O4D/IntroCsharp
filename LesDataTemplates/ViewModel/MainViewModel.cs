7ac97ce7070d351c680ff5e0663d0f3c526084ed
Olivier Mas <olve.onrollover@gmail.com>
2018-10-06 21:12:19 +0200

    Va savoir...

---
+++ LesDataTemplates/ViewModel/MainViewModel.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using LesDataTemplates.Model;
+
+namespace LesDataTemplates.ViewModel
+{
+    class MainViewModel
+    {
+        public List<Square> Elements;
+        public List<int> ElementsInt;
+        public int UnInt = 1;
+        public string UnString = "Lalala";
+
+        public MainViewModel()
+        {
+            Elements = new List<Square>();
+            Elements.Add(new Square());
+            Elements.Add(new Square());
+            Elements.Add(new Square());
+
+            ElementsInt = new List<int>();
+            ElementsInt.Add(0);
+            ElementsInt.Add(1);
+            ElementsInt.Add(2);
+        }
+    }
+}
